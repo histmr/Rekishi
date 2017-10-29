44677a6865f9961162ebb311ec1b896c4f0ab1d7
histmr <mark.ravina@gmail.com>
2017-10-29 00:28:02 -0400

    Initial commit

--- R/Normalize.R
+++ R/Normalize.R
@@ -1,15 +1,14 @@
 #' Kanji normalizing function
 #'
-#' This function converts old 旧漢字 and variant 異体字 Japanese characters to their modern equivalents. 
+#' This function converts old 旧漢字 and variant 異体字 Japanese characters to their modern equivalents.
 #' The conversion table was adapted from # http://wwwap.hi.u-tokyo.ac.jp/ships/itaiji_list.jsp 確認日：2017年10月29日
 #' @param doc A string literal or object
 #' @keywords Japanese, kanji
 #' @export
-#' @examples 
-#' normalize("學")
-#' normalize(text_string)
-#' 
-#' 
+#' @examples
+#' #' normalize(text_string)
+#'
+#'
 normalize <- function(doc)
 {
   kanji_conversion <- data.frame("old" =c("學","勲"
