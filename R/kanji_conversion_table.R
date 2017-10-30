c3f19ce988e88370ea825c039554300868dd922a
histmr <mark.ravina@gmail.com>
2017-10-30 12:37:07 -0400

    Document kanji_conversion_table

---
+++ R/kanji_conversion_table.R
@@ -0,0 +1,13 @@
+#' kanji_conversion_table
+#'
+#' A dataset containing old characters and their modern counterparts. Adapted from The conversion table was adapted from # http://wwwap.hi.u-tokyo.ac.jp/ships/itaiji_list.jsp 確認日：2017年10月29日
+#'
+#'
+#' @format A data frame with 2416 rows and 2 variables:
+#' \describe{
+#'   \item{new}{modern, standard form of character}
+#'   \item{old}{older or variant from}
+#'   ...
+#' }
+#' @source \url{http://wwwap.hi.u-tokyo.ac.jp/ships/itaiji_list.jsp}
+"kanji_conversion_table"
